f443f608f08c48a60378b66a7678e6bf0987972b
mikster36 <74252522+mikster36@users.noreply.github.com>
2022-11-06 23:58:40 -0500

    Add files via upload

---
+++ site.js
@@ -0,0 +1,59 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+const height = 1080;
+const width = 1920;
+
+// resize canvas (CSS does scale it up or down)
+canvas.height = height;
+canvas.width = width;
+
+let drawing = false;
+
+
+function startDrawing(e) {
+    drawing = true;
+    context.beginPath();
+}
+
+window.addEventListener("mousedown", startDrawing);
+
+function endDrawing(e) {
+    drawing = false;
+}
+
+window.addEventListener("mouseup", endDrawing);
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect(),
+      scaleX = canvas.width / rect.width,
+      scaleY = canvas.height / rect.height;
+  
+    return {
+      x: (evt.clientX - rect.left) * scaleX,
+      y: (evt.clientY - rect.top) * scaleY
+    }
+}
+
+function draw(e) {
+    if (!drawing) return;
+
+    let { x, y } = getMousePos(canvas, e);
+
+    context.lineTo(x, y);
+    context.stroke();
+}
+
+window.addEventListener("mousemove", draw);
+
+function startDrawing(e) {
+    drawing = true;
+    context.beginPath();
+    draw(e);
+}
+
+context.lineWidth = 3;
+context.lineCap = "round";
+
+
+
