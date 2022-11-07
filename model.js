78731658fe435a8c922e41a1461726682710e9c3
mikster36 <74252522+mikster36@users.noreply.github.com>
2022-11-07 02:11:29 -0500

    Add files via upload

---
+++ model.js
@@ -0,0 +1,24 @@
+const button = document.getElementById("button");
+button.addEventListener("click", model);
+ 
+
+function getImgFromCanvas() {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    var imgData = context.getImageData(0, 0, 801, 451);
+    return imgData;
+};
+
+
+async function model(){
+    const model = await tf.loadLayersModel("http://127.0.0.1:8080/model.json");
+    const image = getImgFromCanvas();
+    const test = tf.browser.fromPixels(image).expandDims(0, null);
+    console.log(test);
+    const prediction = model.predict(test);
+    const logits = Array.from(prediction.dataSync());
+    document.getElementById("prediction").innerHTML = "There is a " + String(logits[1]) + " chance you have Parkinson's Disease.";
+    
+    
+};
+
